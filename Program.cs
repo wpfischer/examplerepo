69f680ca0b90cc2b59d32169d589d9c18108115e
Will Fischer <willfischer@MacBook-Pro-6.local>
2020-09-03 21:03:08 -0500

    updated welcome message

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace examplerepo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Git is great!");
         }
     }
 }
